a73d36f18abb6b42c30125701590b1df6901b1a6
LUCAS ALEXANDRE DA SILVA <25203702@CFP127ALUNO.LOCAL>
2026-03-20 17:06:27 -0300

    feat: adiciona novas funções ao arquivo de funções.js

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -7,23 +7,142 @@
 const dadosEstadoCidade = require('./estados_cidades')
 
 const listaBrasil = dadosEstadoCidade
-const listaUf = []
 
-const getListaDeEstados = function(){
-    
-    // percorro o JSON de estados
-    listaBrasil.listaDeEstados.estados.forEach(function(item){
-        console.log(item.sigla)
-        
-        let soma = 0
-        for(let i = 0; i < item.sigla.length; i++){
-            soma += item.sigla[i] 
+const getListaDeEstados = function () {
+    let listaUfArray = []
+    let listaResposta
 
-            console.log(soma)
-        }
-
+    listaBrasil.listaDeEstados.estados.forEach(function (itemEstado) {
+        listaUfArray.push(itemEstado.sigla)
     })
 
+    let quantidade = listaUfArray.length
+
+    if (listaUfArray.length > 0 && quantidade > 0) {
+
+        listaResposta = {
+            "uf": listaUfArray,
+            "quantidade": quantidade
+        }
+
+        return listaResposta
+
+    } else {
+        return false
+    }
 }
 
-getListaDeEstados()
+const getDadosEstado = function (siglaEstado) {
+
+    let resposta = {
+        uf: null,
+        descricao: null,
+        capital: null,
+        regiao: null
+    }
+
+    listaBrasil.listaDeEstados.estados.forEach(function (itemEstado) {
+
+        if (siglaEstado.toLowerCase() == itemEstado.sigla.toLowerCase()) {
+
+            resposta.uf = itemEstado.sigla
+            resposta.descricao = itemEstado.nome
+            resposta.capital = itemEstado.capital
+            resposta.regiao = itemEstado.regiao
+        }
+    })
+
+    if (resposta.uf == null      || resposta.descricao == null ||
+        resposta.capital == null || resposta.regiao == null) {
+
+        return false
+
+    }else{
+        return resposta
+    }
+}
+
+const getCapitalEstado = function(siglaEstado){
+
+    let resposta = {
+        uf: null,
+        descricao: null,
+        capital: null
+    }
+
+    listaBrasil.listaDeEstados.estados.forEach(function(itemEstado){
+
+        if(siglaEstado.toLowerCase() == itemEstado.sigla.toLowerCase()){
+
+            resposta.uf        = itemEstado.sigla
+            resposta.descricao = itemEstado.nome
+            resposta.capital   = itemEstado.capital
+        }
+    })
+
+    if(resposta.uf == null || resposta.descricao == null||resposta.capital == null){
+        return false
+    
+    }else{
+        return resposta
+    }
+}
+
+const getEstadosRegiao = function(regiao){
+
+    let resposta = {
+        regiao: null,
+        estados: null
+    }
+
+    let estadosArray = []
+
+    listaBrasil.listaDeEstados.estados.forEach(function(itemEstado){
+        
+        if(regiao.toLowerCase() == itemEstado.regiao.toLowerCase()){
+            
+            resposta.regiao  = itemEstado.regiao
+            estadosArray.push({uf: itemEstado.sigla, descricao: itemEstado.nome})
+        }
+    })
+
+    resposta.estados = estadosArray
+
+    if(resposta.regiao == null || resposta.estados == null){
+        return false
+    
+    }else{
+        return resposta
+    }
+}
+
+const getCapitalPais = function(){
+
+    let resposta = {
+        capitais: null
+    }
+
+    let capitaisArray = []
+
+    listaBrasil.listaDeEstados.estados.forEach(function(itemEstado){
+        
+        if(itemEstado.capital_pais){
+
+            capitaisArray.push({capital_atual: itemEstado.capital_pais.capital, uf: itemEstado.sigla, descricao: itemEstado.nome, 
+                                capital: itemEstado.capital, regiao: itemEstado.regiao, capital_pais_ano_inicio: itemEstado.capital_pais.ano_inicio,
+                                capital_pais_ano_terminio: itemEstado.capital_pais.ano_fim
+           
+            })
+        }
+    })
+
+    resposta.capitais = capitaisArray
+
+    if(resposta.capitais == null){
+        return false
+
+    }else{
+        return resposta
+    }
+}
+
